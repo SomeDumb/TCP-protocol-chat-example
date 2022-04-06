4489ffb7f94bf4e60d55a86c27847b7229e28dbc
Elnur <gulievelnur@gmail.com>
2022-04-06 23:54:35 +0300

    basic auth

--- client.py
+++ client.py
@@ -1,25 +1,185 @@
-from socket import *
+import socket
 import sys
+import struct
+import threading
+import time
 
-host = 'localhost'
-port = 2020
-addr = (host,port)
+class Client:
+    def __init__(self):
+        """ LOGIN STATES """
+        self.register_answers = ["GET LOGIN", 'LOGIN EXISTS', 'LOGIN ACCEPTED', 'REGISTER PASS', 'PASSWORD ACCEPTED']
+        self.login_answers = ["AUTH LOGIN", "WRONG AUTH", "UNEXISTED LOGIN", "AUTH PASS", "LOGIN SUCCESSFUL"]
+        self.messanger_answers = ["GET MEMBERS", "NOT VALID LIST", "SEND CHATERS", "CONNECT TO THE CHAT", "CHOOSE CHAT", "NO USERS", "SEND CHAT"]
+        self.chat_answers = ["MESSAGE FROM"]
+        host = socket.gethostname()
+        port = 2120
+        self.addr = (host,port)
+        
+        connected = self.connect()
+        self.listen_thread = threading.Thread(target=self.listen)
+        self.main_menu_thread = threading.Thread(target=self.main_menu)
+        self.messanger_menu_thread = threading.Thread(target=self.messanger_menu)
+        if connected:
 
-tcp_socket = socket(AF_INET, SOCK_STREAM)
-tcp_socket.connect(addr)
+            self.listen_thread.start()
+            self.main_menu_thread.start()
+    
+    def connect(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect(self.addr)
+        except:
+            command = input('Не удалось получить доступ к серверу, повторить попытку? Д/Н:')
+            if command.lower() == 'д':
+                self.connect()
+            else:
+                return False
+                
+        
+    
+    def main_menu(self):
+        while True:
+            ''' Главное меню для регистрации и авторизации '''
+            
+            try:
+                command = input('Комманды: 1. Регистрация\n 2. Авторизация \n команда: ')
+                if  command == '1':
+                    self.send_one_message(b'REGISTER')
+                    break
+                elif command == '2':
+                    self.send_one_message(b'LOGIN')
+                    break
+                else:
+                    self.send_one_message(command.encode('utf-8'))
+                    
+            except Exception as e:
+                print(e)
+                print('connection is over')
+                break
+    
+    def messanger_menu(self):
+        while True:
+            ''' Меню мессенджера '''
+            try:
+                command = input('Комманды: 1. Создать чат \n 2. Присоеденится существуещему чату\n Комманда: ')
+                if  command == '1':
+                    self.send_one_message(b'SEND CHATERS')
+                    self.send_one_message(b'NEW CHAT')
+                    break
+                if command == '2':
+                    self.send_one_message(b'JOIN CHAT')
+                    break
+            except:
+                pass
+    
+    def messanger(self, message):
+        if message == 'GET MEMBERS':
+            members = input('Введите список участников чата: ')
+            self.send_one_message(members.encode())
+        elif message == 'NOT VALID LIST':
+            members = input('Введите корректный список: ')
+            self.send_one_message(members.encode())
+        elif message == 'CONNECT TO THE CHAT':        
+            th = threading.Thread(target=self.chat_menu)
+            th.start()
+        elif message == 'SEND CHAT':
+            i = str(input('Выберите чат!: '))
+            self.send_one_message(i.encode())
+        elif message == 'NO USERS':
+            print('Нет пользователей для запуска чата! Подождите!')
+        elif message == 'CHOOSE CHAT':
+            pass
+        
+    def chat_menu(self):
+        print('Начало чата!')
+        while True:
+            message = input('')
+            self.send_one_message(message.encode())
+            
+    def listen(self):
+        while True:
+            try:
+                message = self.recv_one_message(self.socket).decode('utf-8')
+                #print(f'\n MESSAGE from server: "{message}"')
+                if message in self.register_answers:
+                    self.register(message)
+                elif message in self.login_answers:
+                    self.login(message)
+                elif message in self.messanger_answers:
+                    self.messanger(message)
+                elif message == 'MESSAGE FROM':
+                    sender = self.recv_one_message(self.socket).decode('utf-8')
+                    print(f'Сообщение от {sender}: ')
+                else:
+                    print(message)
+            except:
+                pass
+    
+    def login(self, message):
+        if message == "AUTH LOGIN":
+            login = input('Введите логин: ')
+            self.send_one_message(login.encode())
+        elif message == "AUTH PASS":
+            password = input('Введите пароль: ')
+            self.send_one_message(password.encode())
+        elif message == "LOGIN SUCCESSFUL":
+            print("Успешный вход!")
+            self.messanger_menu_thread.start()
+        elif message == "WRONG AUTH":
+            print("Неверный логин или пароль, повторите попытку.")
+            self.send_one_message(b'LOGIN')
 
+    def register(self, message):
+        if message == 'GET LOGIN':
+            login = input('Придумайте логин: ')
+            while not login:
+                login = input(('Придумайте корректный логин: '))
+            self.send_one_message(login.encode())
+        elif message == 'LOGIN EXISTS':
+            print('Логин существует.')
+            login = input('Придумайте другой: ')
+            while not login:
+                login = input('Придумайте другой: ')
+            self.send_one_message(login.encode())
+        elif message == 'LOGIN ACCEPTED':
+            print('Логин принят!')
+        elif message == 'REGISTER PASS':
+            password = input('Придумайте пароль: ')
+            self.send_one_message(password.encode())
+        elif message == 'PASSWORD ACCEPTED':
+            print('Пароль принят')
+            self.messanger_menu_thread.start()
+        else:
+            password = input('Придумайте пароль: ')
+            while not password:
+                password = input('Придумайте пароль: ')
+            self.send_one_message(password.encode())         
 
-data = input('write to server: ')
-if not data : 
-    tcp_socket.close() 
-    sys.exit(1)
-
-#encode - перекодирует введенные данные в байты, decode - обратно
-data = str.encode(data)
-tcp_socket.send(data)
-data = bytes.decode(data)
-data = tcp_socket.recv(1024)
-print(data)
-
-
-tcp_socket.close()
+    def get_chaters(self):
+        chaters = False
+        while not chaters:
+            self.send_one_message(b'\nget_chaters')
+            chaters = self.tcp_socket.recv(1024)
+        return chaters.decode()
+    
+    def send_one_message(self, data):
+        length = len(data)
+        self.socket.sendall(struct.pack('!I', length))
+        self.socket.sendall(data)
+        #print('MESSAGE SENDED: ', data)
+        
+    def recvall(self, sock, count):
+        buf = b''
+        while count:
+            newbuf = sock.recv(count)
+            if not newbuf: return None
+            buf += newbuf
+            count -= len(newbuf)
+        return buf
+    
+    def recv_one_message(self, sock):
+        lengthbuf = self.recvall(sock, 4)
+        length, = struct.unpack('!I', lengthbuf)
+        return self.recvall(sock, length)
+    
+client = Client()
--- server.py
+++ server.py
@@ -1,45 +1,261 @@
 #Модуль socket для сетевого программирования
-from socket import *
+import socket
+import threading
+import shortuuid
+import bcrypt
+import struct
+import json
+import os
+def find_key(d, value):
+    for k,v in d.items():
+        if isinstance(v, dict):
+            p = find_key(v, value)
+            if p:
+                return [k] + p
+        elif v == value:
+            return [k]
 
-#данные сервера
-host = 'localhost'
-port = 2020
-addr = (host,port)
+class Server:
+    def __init__(self, ip, port):
+        self.host = ip
+        self.port = port
+        addr = (self.host,self.port)
+        if os.path.exists('chats.json'):
+            self.chats = self.from_json('chats.json')
+        else:
+            self.chats = {}
+            open('chats.json', mode='a').close()
+        
+        if os.path.exists('users.json'):
+            self.users = self.from_json('users.json')
+        else:
+            self.users = {}
+            open('users.json', mode='a').close()
+        
 
-#socket - функция создания сокета 
-#первый параметр socket_family может быть AF_INET или AF_UNIX
-#второй параметр socket_type может быть SOCK_STREAM(для TCP) или SOCK_DGRAM(для UDP)
-tcp_socket = socket(AF_INET, SOCK_STREAM)
-#bind - связывает адрес и порт с сокетом
-tcp_socket.bind(addr)
-#listen - запускает прием TCP
-tcp_socket.listen(1)
+        
+        self.connections = {}
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind(addr)
+            self.socket.listen()
+            print(f"Server started on <{ip}:{port}>")
+            self.bind()
+        except Exception as e:
+            print(f'Unable to listen <{ip}:{port}>')
+            print(e)
+        
 
-#Бесконечный цикл работы программы
-while True:
+    def create_uid(self, dir):
+        uid = shortuuid.ShortUUID().random(length=4)
+        if uid in dir:
+            while uid in self.dir:
+                uid = shortuuid.ShortUUID().random(length=4)
+        return str(uid)
     
-    #Если мы захотели выйти из программы
-    question = input('Do you want to quit? y\\n: ')
-    if question == 'y': break
+    def create_chat(self, admin, users):
+        uid = self.create_uid(self.chats)
+        self.chats[uid] = {'users':users, 'admin':admin}
+        self.save_dict(self.chats,'chats.json')
+        return uid
     
-    print('wait connection...')
+    @staticmethod
+    def from_json(file):
+        if os.path.getsize(file) > 0:
+            with open(file) as json_file:
+                data = json.load(json_file)
+            return data
+        else:
+            return {}
     
-    #accept - принимает запрос и устанавливает соединение, (по умолчанию работает в блокирующем режиме)
-    #устанавливает новый сокет соединения в переменную conn и адрес клиента в переменную addr
-    conn, addr = tcp_socket.accept()
-    print('client addr: ', addr)
+    def get_user_chats(self, login):
+        pass
     
-    #recv - получает сообщение TCP
-    data = conn.recv(1024)
-    #если ничего не прислали, завершим программу
-    if not data:
-        conn.close()
-        break
-    else:
-        print(data)
-        #send - передает сообщение TCP
-        conn.send(b'Hello from server!')
-        #close - закрывает сокет
-        conn.close()
+    @staticmethod
+    def save_dict(dictionary,filename):
+        with open(filename, "w") as outfile:
+            json.dump(dictionary, outfile)
     
-tcp_socket.close()
+    def register(self, conn, address, message):
+        try:
+            self.send_one_message(conn, b'GET LOGIN')
+            while True:
+                login = self.recv_one_message(conn).decode('utf-8')
+                uid=self.create_uid(self.users)
+                if login in [u['login'] for u in self.users.values()]:
+                    self.send_one_message(conn, b'LOGIN EXISTS')
+                else:
+                    self.send_one_message(conn, b'LOGIN ACCEPTED')
+                    break
+            
+            self.send_one_message(conn, b'REGISTER PASS')
+            password = self.recv_one_message(conn)
+            salt = bcrypt.gensalt()
+            hashed = bcrypt.hashpw(password, salt)
+            self.users[uid] = {'login':login, 'password':hashed.decode(), 'chats':None}
+            self.send_one_message(conn, b'PASSWORD ACCEPTED')
+            self.save_dict(self.users,'users.json')
+            print(self.users)
+            self.connections[uid]=conn
+            th = threading.Thread(target=self.listen_user,args=(conn, address, uid))
+            th.start()
+        except Exception as e:
+            print(e)
+            print('registration failed')
+    
+    def listen_anonim(self, conn, address):
+        """ Прослушивание анонимного чатера """
+        while True:
+            try:
+                message = self.recv_one_message(conn).decode('utf-8')
+                if message == 'REGISTER':
+                    self.register(conn,address, message)
+                    break
+                elif message == 'LOGIN':
+                    self.send_one_message(conn,b'AUTH LOGIN')
+                    login = self.recv_one_message(conn).decode('utf-8')
+                    if login in [u['login'] for u in self.users.values()]:
+                        uid = find_key(self.users, login)[0]
+
+                        self.send_one_message(conn,b'AUTH PASS')
+                        passwd = self.recv_one_message(conn)
+                        if bcrypt.checkpw(passwd, self.users[uid]['password'].encode()):
+                            self.send_one_message(conn,b'LOGIN SUCCESSFUL')
+                            self.connections[uid]=conn
+                            th = threading.Thread(target=self.listen_user,args=(conn, address, uid))
+                            th.start()
+                            break
+                        else:
+                            self.send_one_message(conn,b'WRONG AUTH')
+                    else:
+                        self.send_one_message(conn,b'AUTH PASS')
+                        self.send_one_message(conn,b'WRONG AUTH')
+            except Exception as e:
+                #print(e)
+                print(f"On {str(address)} connection is dead")
+                break
+            
+    def listen_chat(self, conn, address, chat, user_uid):
+        while True:
+            try:
+                message = self.recv_one_message(conn).decode('utf-8')
+                message_send = self.users[user_uid]['login']+ ':' + message
+                for chater in chat['users']:
+                    try:
+                        if chater != user_uid:
+                            self.send_one_message(self.connections[chater], message_send.encode())
+                    except:
+                        print('User not connected')
+                        continue
+            except Exception as e:
+                print('User disconnected')
+                break
+    
+    def existing_chat(self, users):
+        for u in self.chats.values():
+            if set(u['users']) == users:
+                return u
+        return False
+                
+    
+    def listen_user(self, conn, address, uid):
+        self.connections[uid] = conn
+        me = self.users[uid]
+        user_uid = uid
+        while True:
+            try:
+                message = self.recv_one_message(conn).decode()
+                if message == 'NEW CHAT':
+                    self.send_one_message(conn,b'GET MEMBERS')
+                    users = self.recv_one_message(conn).decode('utf-8').split(',')
+                    print(users)
+                    for user in users:
+                        if not user in [u['login'] for u in self.users.values()]:
+                            self.send_one_message(conn, b'NOT VALID LIST')
+                    else:
+                        self.send_one_message(conn, b'CONNECT TO THE CHAT')
+                        users_uids = [find_key(self.users, user)[0] for user in users]
+                        users_uids.append(uid)
+                        print(users_uids)
+                        uid = self.existing_chat(users_uids)
+                        if not uid:
+                            uid = self.create_chat(user, users_uids)
+                            self.send_one_message(conn, b'CONNECT TO THE CHAT')
+                            th = threading.Thread(target=self.listen_chat,args=(conn, address, uid, user_uid))
+                            th.start()
+                            break
+                        else:
+                            self.send_one_message(conn, b'CONNECT TO THE CHAT')
+                            th = threading.Thread(target=self.listen_chat,args=(conn, address, uid, user_uid))
+                            th.start()
+                            break
+                elif message == 'SEND CHATERS':
+                    chaters = [u['login'] for u in self.users.values() if me['login']!=u['login']]
+                    if len(chaters)>1:
+                        chaters = ', '.join(chaters)
+                        message = f'Количество пользователей: {len(self.users)}. \n Пользователи: {chaters}'
+                        self.send_one_message(conn, message.encode())
+                    else:
+                        self.send_one_message(conn, b"NO USERS")
+                elif message == 'JOIN CHAT':
+                    chats = []
+                    for chat in self.chats.values():
+                        if uid in chat['users']:
+                            chats.append(chat)
+                    message = ''
+                    for i in range(len(chats)):
+                        chat = chats[i]
+                        message+=f'\n{str(i)}: '
+                        users = ",".join([self.users[u]['login'] for u in chat['users']])
+                        message+=users
+                    self.send_one_message(conn, message.encode())
+                    self.send_one_message(conn, b'SEND CHAT')
+                    i = self.recv_one_message(conn).decode('utf-8')
+                    uid = chats[int(i)]
+                    self.send_one_message(conn, b'CONNECT TO THE CHAT')
+                    th = threading.Thread(target=self.listen_chat,args=(conn, address, uid, user_uid))
+                    th.start()
+                    break
+                else:
+                    print(message)        
+            except Exception as e:
+                print(e)
+                print(f"On {str(address)} connection is dead")
+                break
+            
+    def bind(self):
+        while True:
+            conn, address = self.socket.accept()
+            th = threading.Thread(target=self.listen_anonim,args=(conn, address))
+            th.start()
+            print("NEW USER JOIND")
+            
+    def send_one_message(self, sock, data):
+        try:
+            length = len(data)
+            sock.sendall(struct.pack('!I', length))
+            sock.sendall(data)
+            print('MESSAGE SENDED: ', data)
+        except:
+            pass
+        
+    def recvall(self, sock, count):
+        buf = b''
+        while count:
+            newbuf = sock.recv(count)
+            if not newbuf: return None
+            buf += newbuf
+            count -= len(newbuf)
+        return buf
+
+    
+    def recv_one_message(self, sock):
+        try:
+            lengthbuf = self.recvall(sock, 4)
+            length, = struct.unpack('!I', lengthbuf)
+            return self.recvall(sock, length)
+        except:
+            pass
+
+ip = input('Введите ip сервера: ')
+serv = Server(ip, 2120)
